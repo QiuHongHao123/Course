2910caea8babd9c12292195f6d90147bfee2c735
Qiuhongh <49175046+qgreenhand@users.noreply.github.com>
2019-12-06 11:04:06 +0800

    Add files via upload

---
+++ Tensorflowstudy/convolution_network/LeNet5_test.py
@@ -0,0 +1,40 @@
+import tensorflow as tf
+from tensorflow.keras import Sequential,layers
+
+class my_nn_layer(tf.keras.layers.Layer):
+    def __init__(self,kernel_size,inputdim,outdim):
+        super(my_nn_layer, self).__init__()
+        self.w=tf.Variable(tf.random.normal([kernel_size,kernel_size,inputdim,outdim]))
+
+    def __call__(self, input):
+        out = tf.nn.conv2d(input, self.w, padding='SAME', strides=1)
+        return out
+
+class Mynetwork(tf.keras.Model):
+    def __init__(self):
+        super(Mynetwork, self).__init__()
+        self.network=Sequential([
+        my_nn_layer(3,1,6),
+        #layers.Conv2D(6, kernel_size=3, strides=1),  # 第一个卷积层, 6 个 3x3 卷积核
+        tf.keras.layers.MaxPooling2D(pool_size=2,strides=2), # 高宽各减半的池化层
+        tf.keras.layers.ReLU(),
+        my_nn_layer(3, 6,16),
+        #layers.Conv2D(16, kernel_size=3, strides=1),  # 第二个卷积层, 16 个 3x3 卷积核
+        tf.keras.layers.MaxPooling2D(pool_size=2, strides=2),  # 高宽各减半的池化层
+        tf.keras.layers.ReLU(), # 激活函数
+        tf.keras.layers.Flatten(),  # 打平层，方便全连接层处理
+
+        tf.keras.layers.Dense(120, activation='relu'),
+        # 全连接层，120 个节点
+        tf.keras.layers.Dense(84, activation='relu'), # 全连接层，84 节点
+        tf.keras.layers.Dense(10) # 全连接层，10 个节点
+        ])
+        self.network.build(input_shape=[4, 28, 28, 1])
+        print(self.network.summary())
+    def __call__(self, inputs):
+        x=self.network(inputs)
+        return x
+Mynetwork=Mynetwork()
+
+
+print(network.summary())
